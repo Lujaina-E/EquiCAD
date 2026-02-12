2eb03b56b007c7e7d82d7d16aeb1f0e66fa3c9b2
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2026-02-12 10:17:13 -0700

    modifying cors permissions

--- app/backend/app.py
+++ app/backend/app.py
@@ -15,23 +15,15 @@ import uuid
 from flask import send_from_directory
 from fastapi.middleware.cors import CORSMiddleware
 
-
-origins = [
-    "http://localhost:3000",        
-    "http://127.0.0.1:3000",        
-    "http://frontend:3000",         
-    "https://netlify-equicad-frontend.netlify.app",  
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,         
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app = Flask(__name__)
+
+# origins = [
+#     "http://localhost:3000",        
+#     "http://127.0.0.1:3000",        
+#     "http://frontend:3000",         
+#     "https://netlify-equicad-frontend.netlify.app",  
+# ]
+
 CORS(
     app,
     origins=[
