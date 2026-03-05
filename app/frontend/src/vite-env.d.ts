63417de662378cb3291f1a87a76f9c1271a3d859
Lujaina Eldelebshany <65575947+Lujaina-E@users.noreply.github.com>
2026-03-05 01:09:03 -0700

    allowing permissions from file import

--- app/frontend/src/vite-env.d.ts
+++ app/frontend/src/vite-env.d.ts
@@ -5,3 +5,10 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+/// <reference types="vite/client" />
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
